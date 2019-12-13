61232bd2be9d5f226e0a089fb9ca2bef4ae61382
niezhichao <727841296@qq.com>
2019-12-13 17:22:16 +0800

    1213 1722

---
+++ hrmWeb/WebContent/common/jsLoader.js
@@ -0,0 +1,28 @@
+var AppConf={
+    resourceRoot:"../resource/",
+    cssRoot:"../css/",
+    jsRoot:"../js/"
+}
+
+/**/
+function getPageName() {
+    var url=location.href;
+    var arr=url.split("/");
+    var nameHasSuffix=arr[arr.length-1];
+    var name=nameHasSuffix.substring(0,nameHasSuffix.indexOf("."));
+    return name;
+}
+
+function loadCss(path) {
+    document.write('<link rel="stylesheet" type="text/css" href="'+path+'" />');
+}
+
+function loadJs(path) {
+    document.write('<script src="'+path+'"></script>');
+}
+
+loadJs(AppConf.resourceRoot+"jquery/jquery-3.3.1.js");
+loadJs(AppConf.jsRoot+getPageName()+".js");
+loadCss(AppConf.cssRoot+getPageName()+".css");
+
+
